eb63a5dc8c1459aad54d822f0e030ed6ba0b467e
rajkumar963 <rajkumarsaw597@gmail.com>
2024-04-30 18:47:26 +0530

    update

--- Redux/Redux-Class/src/app/store.js
+++ Redux/Redux-Class/src/app/store.js
@@ -1,5 +1,7 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit';
+import todoReducer from '../Features/Todo/todoSlice';
 
 export const store= configureStore({
-  reducer: {},
-})
+  reducer: todoReducer,
+});
+
